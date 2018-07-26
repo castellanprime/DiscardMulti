5c3f1d8d7c17cc5c3c9ce193409ea57fc9431678
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2018-07-26 20:44:24 +0000

    Added model file

---
+++ model.py
@@ -0,0 +1,64 @@
+import pickle, logging, os
+
+state_db_file = 'state.db'
+
+class Model(object):
+	def __init__(self):
+		self.__logger = logging.getLogger(__name__) 
+		self.hand = []
+		self.last_played = []
+		#self.__set_previous_decks()
+
+	def _findDBfiles(self):
+		self.__logger.info("Finding database file")
+		for root, dirs, files in os.walk(os.getcwd()):
+			for file in files:
+				if file.endswith(".db") and file == state_db_file:
+					return(os.path.join(root,file))
+
+	def __set_previous_decks(self):
+		filename = self._findDBfiles()
+		state = self.load(filename)
+		if state:
+			self.hand = state['hand']
+			self.last_played = state['last_played']
+
+	def get_hand(self):
+		return self.hand
+
+	def get_last_played(self):
+		return self.last_played
+
+	def add_card(self, card):
+		# Adding new card at the bottom of the hand
+		self.hand.append(card)
+
+	def add_cards(self, cards):
+		self.hand.extend(cards)
+
+	def pick_card(self, index):
+		self.last_played.append(self.hand.pop(index))
+		return self.last_played[len(self.last_played) - 1]
+
+	def save(self):
+		self.__logger.info('Saving current state!!')
+		filename = self._findDBfiles()
+		state = {}
+		state['hand'] = self.hand
+		state['last_played'] = self.last_played
+		file = open(filename, 'wb')
+		pickle.dump(state, file)
+		file.close()
+
+	def load(self, filename):
+		self.__logger.info("Loading old state!!")
+		state = {}
+		if os.path.getsize(filename) > 0:
+			file = open(filename, 'rb')		
+			try:
+				state = pickle.load(file)
+			except(EOFError):
+				self.__logger.debug("File is empty")
+				state = {}
+			file.close()
+		return state
