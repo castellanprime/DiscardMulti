17f8d5eece471605df50a9b75c4be2e31830fbea
Okusanya Damilola <okusanya.damilola@gmail.com>
2018-07-28 20:10:20 -0400

    Added comments and removed incorrect references. Added DiscardMessage to replace ordinary dict used for message.

--- playerclient2.py
+++ playerclient2.py
@@ -4,28 +4,40 @@
 	# yield a Future returns a result
 """
 
-from tornado import ioloop, websocket, gen
-from model import Model
-from players import Human
-from serverenums import ClientEnum
+
 import requests
 import json
 import sys
 import uuid
+import logging
+
+from tornado import ioloop, websocket, gen
+
+from model import Model
+from players import Human
+from serverenums import ( ClientRcvMessage, RoomGameStatus,
+		RoomRequest)
+from utils import DiscardMessage
+
 
 class PlayerController(object):
+
 	def __init__(self, room_url, game_url):
 		self.wsconn = None
 		self.player = None
 		self.model = Model()
 		self.wsconn_close = False
-		self.has_connected = False
 		self.room_url = room_url 
 		self.game_url = game_url 
 		self.has_initialised = False 
-		self.game_has_started = False
 
 	def get_str_input(self, question):
+		"""
+		This gets a string input
+
+		:param question: Prompt for user to give an answer to
+		:returns: str -- User answer
+		"""
 		choice = input(question)
 		while any(( choice is None, 
 			not choice.strip() )):
@@ -34,6 +46,12 @@ class PlayerController(object):
 		return choice
 
 	def get_int_input(self, question):
+		"""
+		This gets a integer
+
+		:param question: Prompt for user to give an answer to
+		:returns: int -- User answer
+		"""
 		while True:
 			choice = input(question)
 			try:
@@ -43,6 +61,7 @@ class PlayerController(object):
 				print(err)
 
 	def create_new_user(self):
+		"""	This creates a new Human user """
 		user_id = uuid.uuid4().hex 
 		self.player = Human(user_id, self, self.model)
 
@@ -51,6 +70,7 @@ class PlayerController(object):
 		self.player.set_nickname(username)
 
 	def create_room(self):
+		""" This creates a room	"""
 		print("=== Creating a room ===")
 		self.create_new_user()
 		question = 'How many players do you want to play with:'
@@ -61,30 +81,36 @@ class PlayerController(object):
 		msg = {'username': self.player.get_nickname(), \
 		 	'userid': self.player.get_user_id(), \
 			'num_of_players':num_of_players}
-		param = {'cmd': 'CREATE_A_ROOM'}
+		param = {'cmd': RoomRequest.CREATE_A_ROOM.value}
 		req = requests.post(self.room_url, 
 			json=msg, params=param)
-		response = json.loads(req.text)
-		print("Your new room id=", response['prompt'])
-		self.player.set_room_id(response['prompt'])
+		response = DiscardMessage.to_obj(req.text)
+		print("Your new room id=", response.get_data())
+		self.player.set_room_id(response.get_data())
 
 	def find_room(self):
+		""" 
+		This find rooms 
+
+		:returns: bool -- True if rooms has been found. False if otherwise
+		"""
 		print('==== Getting rooms to play the game ====')	
 		return_value = False
-		param = {'cmd': 'GET_ROOMS'}
+		param = {'cmd': RoomRequest.GET_ROOMS.value}
 		req = requests.get(self.room_url, 
 			params=param)
-		rooms = json.loads(req.text)
-		if rooms['lis']:
+		rooms = DiscardMessage.to_obj(req.text)
+		if rooms.get_data():
 			ls = [str(ind)+') '+ str(value) 
-				for ind, value in enumerate(rooms['lis'])
+				for ind, value in enumerate(rooms.get_data())
 			]
 			room_str = '\n'.join(ls)
 			print('The rooms available:', '\n', room_str)
 			choice = self.get_int_input('Choose room to join: ')
 			while choice >= len(ls):
 				choice = self.get_int_input('Choose room to join: ')
-			room = rooms['lis'][choice]
+			room_ = rooms.get_data()
+			room = room_[choice]
 			print(room)
 			
 			self.create_new_user()
@@ -92,26 +118,31 @@ class PlayerController(object):
 			self.player.set_room_id(room['roomid'])
 			print('You selected: ', self.player.get_room_id())
 			
-			param = { 'cmd': 'JOIN'}
-			msg = {'username': self.player.get_nickname(), \
-				'userid':self.player.get_user_id(), \
-				'roomid': room['roomid']}
+			param = { 'cmd': RoomRequest.JOIN_ROOM.value }
+			msg = {
+				'username': self.player.get_nickname(), 
+				'userid':self.player.get_user_id(), 
+				'roomid': room['roomid']
+			}
 			req = requests.post(self.room_url, 
 				json=msg, params=param)
-			response = json.loads(req.text)
+			response = DiscardMessage.to_obj(req.text)
 			print(response)
 			return_value = True 
 		return return_value 
 
 	def show_roomates(self):
-		param = {'cmd': 'GET_ROOMATES',
+		""" This shows roomates """
+		param = {
+			'cmd': RoomRequest.GET_ROOMATES.value,
 			'userid':self.player.get_user_id(),
-			'roomid':self.player.get_room_id()}
+			'roomid':self.player.get_room_id()
+		}
 		req = requests.get(self.room_url, 
 			params=param)
-		response = json.loads(req.text) 
+		response = DiscardMessage.to_obj(req.text)
 		ls = [str(ind)+') '+value for ind,
-				value in enumerate(response['roomates'])]
+				value in enumerate(response.get_data())]
 		if ls:
 			room_str = '\n'.join(ls)
 			print('My roomates:', '\n', room_str)
@@ -119,6 +150,7 @@ class PlayerController(object):
 			print('You have no roomates yet!!')
 
 	def negotiate(self):
+		""" This finds or creates a room """
 		print('==== Starting game ====')
 
 		success = self.find_room()
@@ -126,69 +158,83 @@ class PlayerController(object):
 			print("Can't find any rooms")
 			self.create_room()
 
-		"""
-		question = 'Do you want to show your roomates(y/n)?: '
-		choice = self.get_str_input(question)
-		if choice == 'y':
-			self.show_roomates()
-		"""
-
-	def ping(self):
-		msg = {'cmd':'ARE_ROOMATES_IN_GAME',
-			'roomid': self.player.get_room_id(),
-			'userid': self.player.get_user_id()}
-		return json.dumps(msg)
+	def gen_ping(self):
+		""" 
+		This generates a ping message to send to 
+		the server to ask if roomates can actually
+		start playing the game
+		""" 
+		msg_ = DiscardMessage(cmd=RoomGameStatus.ARE_ROOMATES_IN_GAME.value,
+			data={
+				'roomid': self.player.get_room_id(),
+				'userid': self.player.get_user_id()
+			}
+		)
+		return DiscardMessage.to_json(msg_)
 
 	def start_game(self):
-		msg = {'userid':self.player.get_user_id(), \
-			'roomid':self.player.get_room_id()}
-		param = {'cmd': 'START_GAME'}
+		""" This sends a START_GAME request to game server """
+		msg_ = {
+			'userid':self.player.get_user_id(),
+			'roomid':self.player.get_room_id()
+		}
+		param = {'cmd': RoomRequest.START_GAME.value }
 		req = requests.post(self.room_url, 
-			json=msg, params=param)
-		response = json.loads(req.text)
+			json=msg_, params=param)
+		response = DiscardMessage.to_obj(req.text)
 		print(response)
 
-	def send_test_message(self):
+	def gen_test_message(self):
+		""" This generates a user-generated message """
 		choice = self.get_str_input('Send a message: ')
 		if choice == "End":
-			msg = {}
+			msg_ = {}
+			return json.dumps(msg_)
 		else:
-			msg = {'cmd': 'GAME_MESSAGE',
-				'roomid': self.player.get_room_id(),
-				'userid': self.player.get_user_id(),
-				'message': choice }
-		return json.dumps(msg)
+			msg_ = DiscardMessage(cmd=RoomGameStatus.GAME_MESSAGE.value,
+				data={
+					'roomid': self.player.get_room_id(),
+					'userid': self.player.get_user_id(),
+					'message': choice
+				}
+			)
+			return DiscardMessage.to_json(msg_)
 
 	def generate_wsmessage(self):
+		""" This generates the message to send to the game server """
 		print("[[ In generate_wsmessage ]]")
 		msg = None
 		if self.has_initialised == False:
-			msg = self.ping()
+			msg = self.gen_ping()
 		else:
-			msg = self.send_test_message()
+			msg = self.gen_test_message()
 			if msg == json.dumps({}):
 				return None 
 		return msg
 
-	def handle_msg(self, msg):
+	def handle_msg(self, message):
+		""" Handles messages received from the game server """
 		print("[[ In handle_msg ]]")
-		cmd = ClientEnum[msg['cmd']]
-		if cmd == ClientEnum.GAME_MESSAGE_REP and \
-			msg['payload'][['prompt'] == ClientRcvMessage.value:
+		msg = DiscardMessage.to_obj(message)
+		if all(( msg.cmd == ClientRcvMessage.GAME_MESSAGE_REP.value,
+			msg.get_prompt() == ClientRcvMessage.GAME_CAN_BE_STARTED_REP.value )):
+			print("[[ Starting game ]]")
 			if self.has_initialised == False:
 				self.has_initialised = True
 				self.start_game()
-		elif all(( cmd == ClientRcvMessage.GAME_MESSAGE_REP,
-			msg['payload']['prompt'] == ClientRcvMessage.value )):
+		elif all(( msg.cmd == ClientRcvMessage.GAME_MESSAGE_REP.value,
+			msg.get_prompt() == ClientRcvMessage.GAME_HAS_STARTED_REP.value )):
+			print("[[ Joining stared game ]]")
 			if self.has_initialised == False:
 				self.has_initialised = True
-		elif cmd == ClientEnum.ASK_FOR_ROOMATES:
+		elif msg.cmd== ClientRcvMessage.ASK_FOR_ROOMATES_REP:
 			self.show_roomates()
 		print("Received game message from server: ", msg)
 		
 
 	@gen.coroutine
 	def connect_on_websocket(self):
+		""" This makes a websocket connection to the game server """
 		try:
 			url = self.game_url + '?userId=' + \
 				self.player.get_user_id() + \
@@ -204,6 +250,7 @@ class PlayerController(object):
 
 	@gen.coroutine
 	def send_wsmessage(self):
+		""" This sends a websocket message """
 		print("[[ In send_wsmessage ]]")
 		msg = self.generate_wsmessage()
 		if msg:
@@ -218,6 +265,11 @@ class PlayerController(object):
 
 	@gen.coroutine
 	def communicate_with_websocket(self):
+		""" 
+		This receives messages from the game server and
+		sends back messages to the game server on the 
+		websocket connection 
+		"""
 		print("[[ In communicate_with_websocket ]]")
 		recv_msg = None 
 		while True:
@@ -228,12 +280,13 @@ class PlayerController(object):
 			if recv_msg is None:
 				self.wsconn.close()
 				sys.exit()
-			self.handle_msg(json.loads(recv_msg))
+			self.handle_msg(recv_msg)
 			yield self.send_wsmessage()
 		print("IOLoop terminate")
 
 	@gen.coroutine
 	def main(self):
+		""" Thjs is the entry point for the ioloop """
 		if self.player == None:
 			self.negotiate()
 		yield self.connect_on_websocket()
