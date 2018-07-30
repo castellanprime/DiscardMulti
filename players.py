31b4b3e3efd46485dac909033f5c4e8b00c6825c
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2018-07-30 19:58:06 +0000

    [Experimental] Added some logic for Human play method

--- players.py
+++ players.py
@@ -2,6 +2,8 @@
 	The module is for the Player object
 """
 
+from utils import GameMoveType
+
 class Player(object):
     def __init__(self, user_id):
         self.user_id = user_id
@@ -38,13 +40,16 @@ class Human(Player):
         self.last_card = False
         self.room_id = None
         self._controller = controller
-        self.__prompt = prompt
+        self.__message = None
 
     def set_deck(self, cards):
         self.hand = cards
 
     def set_room_id(self, room_id):
         self.room_id = room_id
+
+    def set_message_to_process(self, msg):
+        self.__mesage_to_process = msg 
 
     def get_room_id(self):
         return self.room_id
@@ -65,7 +70,50 @@ class Human(Player):
         return self.nick
 
     def play(self):
+	"""
+	    Messages to process
+	    cmd: GAME_MESSAGE
+	    data:
+	        prompt: question asked
+	        return_type: GameMoveType.DATATYPE
+	        extra_data: It would normally be the top card
+		nextCmd: Can be used for validation of options	
+	"""
         choice = None
+        msg = {}
+        if self.__message_to_process:
+            prompt = self.__message_to_process.get_payload_value(
+                value='prompt')
+            print(prompt)
+            print(self.__message_to_process.get_payload_value(
+                value='extra_data')
+            if self.__message_to_process.get_payload_value(
+                value='return_type') in [ GameMoveType.DATATYPE_STR.value,
+                    GameMoveType.DATATYPE_INT.value]:
+                if self.__message_to_process.get_payload_value(
+                    value='return_type') == GameMoveType.DATATYPE_STR.value:
+                    choice = self._controller.get_str_input(prompt)
+                    while choice not in self.__message_to_process.get_payload_value(
+                        value='next_cmd'):
+                        print('Wrong_option')
+                        choice = self._controller.get_str_input(prompt)
+                elif self.__message_to_process.get_payload_value(
+                    value='return_type') == GameMoveType.DATATYPE_INT.value:
+                    choice = self._controller.get_int_input(prompt)
+                    while choice not in self.__message_to_process.get_payload_value(
+                        value='next_cmd'):
+                        print('Wrong option!!')
+                        choice = self._controller.get_int_input(prompt)
+                msg = DiscardMessage(cmd=RoomGameStatus.GAME_MESSAGE.value,
+                    data=choice, return_type=self.__message_to_process.get_payload_value(
+                    value='return_type', flag=self.__message_to_process.get_payload_value(
+                    value='flag'))
+                msg.set_id(self.__message_to_process.get_id())
+            elif 
+                    
+           
+            if self.__message_to_process.get_payload_value(
+                value='
         if self.__prompt:
             choice = self._controller.get_str_input(self.__prompt)
         else:
