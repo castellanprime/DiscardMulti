8565179ae0a31386fbae6d9cd22305b23bcd013a
Okusanya Oluwadamilola <okusanya.damilola@gmail.com>
2018-08-01 21:02:42 +0000

    Added some current player and initial player functions

--- servermodel.py
+++ servermodel.py
@@ -6,11 +6,16 @@ class ServerModel(object):
 	def __init__(self, players):
 		self.players = players
 		self.initial_player = None 
+		self.current_player = None
 
 	def get_initial_player(self):
 		return self.initial_player
 
-	def set_initial_player(self, user_id):
-		for player in self.players:
-			if player.user_id == user_id:
-				self.initial_player = player
+	def set_initial_player(self, player):
+		self.initial_player = player
+
+	def set_current_player(self, player):
+		self.current_player = player
+
+	def get_current_player(self):
+		return self.current_player
